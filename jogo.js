b8b994d4e4bbe9c61bc3f5e251d3d4624394d899
Allan David de Oliveira Amorim <allan.j.k@hotmail.com>
2021-04-02 23:35:16 -0300

    Adicionando troca de telas

--- jogo.js
+++ jogo.js
@@ -9,25 +9,25 @@ const ctx = canvas.getContext('2d');
 const background = {
     srcX: 390,
     srcY: 0,
-    largura:275,
-    altura:204,
-    x:0,
-    y:canvas.height - 204,
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
 
-    draw(){
+    draw() {
         ctx.fillStyle = '#70C5CE';
-        ctx.fillRect(0,0, canvas.width, canvas.height);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         ctx.drawImage(
-            sprites, 
+            sprites,
             background.srcX, background.srcY, background.largura, background.altura,
-            background.x, background.y, background.largura,background.altura
+            background.x, background.y, background.largura, background.altura
         );
 
         ctx.drawImage(
-            sprites, 
+            sprites,
             background.srcX, background.srcY, background.largura, background.altura,
-            (background.x + background.largura), background.y, background.largura,background.altura
+            (background.x + background.largura), background.y, background.largura, background.altura
         );
     }
 }
@@ -35,22 +35,22 @@ const background = {
 const floor = {
     srcX: 0,
     srcY: 610,
-    largura:224,
-    altura:112,
-    x:0,
-    y:canvas.height - 112,
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height - 112,
 
-    draw(){
+    draw() {
         ctx.drawImage(
-            sprites, 
+            sprites,
             floor.srcX, floor.srcY, floor.largura, floor.altura,
-            floor.x, floor.y, floor.largura,floor.altura
+            floor.x, floor.y, floor.largura, floor.altura
         );
 
         ctx.drawImage(
-            sprites, 
+            sprites,
             floor.srcX, floor.srcY, floor.largura, floor.altura,
-            (floor.x + floor.largura), floor.y, floor.largura,floor.altura
+            (floor.x + floor.largura), floor.y, floor.largura, floor.altura
         );
     }
 }
@@ -58,42 +58,106 @@ const floor = {
 const flappyBird = {
     srcX: 0,
     srcY: 0,
-    largura:33,
-    altura:24,
-    x:10,
-    y:60,
-    velocidade:0,
-    gravidade:0.25,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 60,
+    velocidade: 0,
+    gravidade: 0.25,
 
-    instantiate(){
+    instantiate() {
         flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
         flappyBird.y = flappyBird.y + flappyBird.velocidade;
 
         flappyBird.draw();
     },
 
-    draw(){
+    draw() {
         ctx.drawImage(
-            sprites, 
+            sprites,
             flappyBird.srcX, flappyBird.srcY, //Sprite X, SpriteY
             flappyBird.largura, flappyBird.altura, //Tamanho do recorte
             flappyBird.x, flappyBird.y, //Posição no canvas
-            flappyBird.largura,flappyBird.altura //Tamanho da imagem dentro do canvas
+            flappyBird.largura, flappyBird.altura //Tamanho da imagem dentro do canvas
         );
     }
 }
 
+const mensagemGetReady = {
+    srcX: 134,
+    srcY: 0,
+    largura: 174,
+    altura: 152,
+    x: (canvas.width / 2) - 174 / 2,
+    y: 50,
 
-function Loop(){
-    background.draw();
-    floor.draw();
+    draw() {
+        ctx.drawImage(
+            sprites,
+            mensagemGetReady.srcX, mensagemGetReady.srcY,
+            mensagemGetReady.largura, mensagemGetReady.altura,
+            mensagemGetReady.x, mensagemGetReady.y,
+            mensagemGetReady.largura, mensagemGetReady.altura
+        );
+    }
+}
 
-    flappyBird.instantiate();
+let currentScreen = {};
 
+function changeScreen(newScreen){
+    currentScreen = newScreen;
+}
 
+const Screens = {
+    JOGO: {
+        draw() {
+            background.draw();
+            floor.draw();
+
+            flappyBird.instantiate();
+        },
+        update() {
+
+        }
+    },
+    GAMEOVER: {
+        draw() {
+
+        },
+        update() {
+
+        }
+    },
+    INICIO: {
+        draw() {
+            background.draw();
+            floor.draw();
+
+            flappyBird.draw();
+
+            mensagemGetReady.draw();
+        },
+        update() {
+
+        },
+        click(){
+            changeScreen(Screens.JOGO);
+        }
+    }
+}
+
+function Loop() {
+    currentScreen.draw();
+    currentScreen.update();
 
     requestAnimationFrame(Loop);
 }
 
+window.addEventListener('click', function(){
+    if(currentScreen.click){
+        currentScreen.click();
+    }
+});
 
+changeScreen(Screens.INICIO);
 Loop();
